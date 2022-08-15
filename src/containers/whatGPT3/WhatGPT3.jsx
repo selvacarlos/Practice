41fff928df08b3366e6e64ba3069c4791800849b
selvacarlos <selvacarlos95@gmail.com>
2022-08-15 09:56:51 -0500

    [GPT3-8] - Created a reusable article component for the 5 articles in th blog section and implemented the footer and final touches befroe deploying.

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Feature } from '../../components'
 import './whatGPT3.css'
 
-const WhatGPT3 = () => {
-  return (
+const WhatGPT3 = () => (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
         <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."/>
@@ -18,7 +17,6 @@ const WhatGPT3 = () => {
         <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
       </div>
     </div>
-  )
-}
+  );
 
 export default WhatGPT3 
